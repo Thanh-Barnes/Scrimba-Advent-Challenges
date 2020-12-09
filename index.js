0f5fd908bf4e460958f488b66c5ff2485c124f2b
Thanh <tdn888@gmail.com>
2020-12-10 00:43:34 +1100

    add challenge 9

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 
 
 // 2. deposit profit challenge - how many years it would take to reach the threshold
-
 function depositProfit(deposit, rate, threshold) {
     let numOfYears = 0;
     let totalAmountAccumulated = 0;
@@ -22,7 +21,6 @@ function depositProfit(deposit, rate, threshold) {
 depositProfit(100, 20, 170) //answer should be 3
 
 // 3. chunky monkey challenge - fnc that splits an array (first arugment) into groups the length of size (second argument) and returns them as 2 dimensional arrayy
-
 function chunkyMonkey(values, size) {
     const endOfLeftOverArr = values.length;
     let selectedArr = [];
@@ -38,7 +36,6 @@ function chunkyMonkey(values, size) {
 chunkyMonkey(['a', 'b', 'c', 'd'], 2) // {['a', 'b'], ['c', 'd']}
 
 // 4. century from year - given a year, return the century it is in. (first century spans from 1 - 100 (included), second century 101 - 200 (included))
-
 function centuryFromYear(num) {
     return Math.ceil(num/100);
 }
@@ -46,7 +43,6 @@ centuryFromYear(2000) //20
 centuryFromYear(200) //17
 
 // 5. reverse a string
-
 //option 1
 // function reverseAString(str) {
 //     let strToArr = str.split("");
@@ -67,7 +63,6 @@ function reverseAString(str) {
 reverseAString('hello'); // 'olleh'
 
 // 6. sort by length - given array of strings, sort in increasing order. If 2 strings have same length, their relative order must be the same as in the initial way.
-
 function sortByLength(strs) {
     
     for (let i = 0; i < strs.length; i++) {
@@ -95,7 +90,6 @@ function sortByLength(strs) {
 sortByLength(["abc", "", "aaa", "a", "zz"]) // ["", "a", "zz", "abc", "aaa"]
 
 // 7. count vowel consonant - given a string that consist of lowercase english letters. if vowels (a e i o u) are given a value of 1 and consonants are given value of 2, returnt he sum of all the letters in the input string.
-
 function countVowelConsonant(str) {
     
     let valueOfLetters = 0;
@@ -121,3 +115,53 @@ countVowelConsonant("abcde") // 8
 
 // 8. rolling dice - 
 console.log('refer to challenge 8 folder')
+
+// 9 . sum odd fibonacci numbers - given a positive integer num, retgurn the sum all odd fibonacci numbers that are thess than or equal to num
+function sumOddFibonacciNumbers(num) {
+    
+    let arr = [];
+    let firstNum = 1;
+    let secondNum = 1;
+    arr.push(firstNum);
+    arr.push(secondNum);
+    
+    for (let i = 0; i < num; i++) {
+        let sumOfFirstNSecondNum = firstNum + secondNum;
+        arr.push(sumOfFirstNSecondNum);
+        firstNum = secondNum;
+        secondNum = sumOfFirstNSecondNum;
+    }
+        
+    let sumOfOddFibonacci = 0;
+    
+    for (let j = 0; j <= num; j++) {
+        
+        if (arr[j] % 2 === 1 && arr[j] <= num) {
+            sumOfOddFibonacci += arr[j];
+        }
+    }
+    return sumOfOddFibonacci;
+}
+// sumOddFibonacciNumbers(10) // 10
+// sumOddFibonacciNumbers(1000) // 1785
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
